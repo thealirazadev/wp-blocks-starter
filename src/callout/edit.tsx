3d25034c64e680c4758d9019728d21a4c3f9be1c
Ali Raza <thealirazadev@gmail.com>
2026-07-17 01:27:12 +0500

    feat(callout): add heading level control to the block toolbar

--- src/callout/edit.tsx
+++ src/callout/edit.tsx
@@ -1,7 +1,14 @@
-import { RichText, useBlockProps } from '@wordpress/block-editor';
+import {
+	BlockControls,
+	HeadingLevelDropdown,
+	RichText,
+	useBlockProps,
+} from '@wordpress/block-editor';
+import { ToolbarGroup } from '@wordpress/components';
 import { createElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
+import { clampHeadingLevel, HEADING_LEVELS } from './constants';
 import CalloutInspector from './controls';
 import type { CalloutAttributes } from './types';
 
@@ -16,14 +23,30 @@ export default function CalloutEdit( {
 	attributes,
 	setAttributes,
 }: CalloutEditProps ) {
-	const { heading, body } = attributes;
+	const { heading, body, headingLevel } = attributes;
 
 	return createElement(
 		'div',
 		useBlockProps( { className: CALLOUT_CLASS } ),
+		createElement(
+			BlockControls,
+			null,
+			createElement(
+				ToolbarGroup,
+				null,
+				createElement( HeadingLevelDropdown, {
+					options: HEADING_LEVELS,
+					value: headingLevel,
+					onChange: ( value: number | undefined ) =>
+						setAttributes( {
+							headingLevel: clampHeadingLevel( value ),
+						} ),
+				} )
+			)
+		),
 		createElement( CalloutInspector, { attributes, setAttributes } ),
 		createElement( RichText, {
-			tagName: 'h2',
+			tagName: `h${ headingLevel }`,
 			className: 'wpbs-callout__heading',
 			value: heading,
 			onChange: ( value: string ) => setAttributes( { heading: value } ),
